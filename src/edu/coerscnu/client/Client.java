de1ca202316c64f0e797734a8064b6d3c8b218c0
Xujie <jie.xu@coer-scnu.org>
2017-06-28 15:25:54 +0800

    完成ArrayUtil类
    1、完成ArrayUtil类，并通过简单测试。

--- src/edu/coerscnu/client/Client.java
+++ src/edu/coerscnu/client/Client.java
@@ -1,21 +1,19 @@
 package edu.coerscnu.client;
 
-import edu.coerscnu.basic.Iterator;
-import edu.coerscnu.basic.list.LinkedList.MyLinkedList;
+import edu.coerscnu.basic.list.ArrayList.ArrayUtil;
 
 public class Client {
 
 	public static void main(String[] args) {
-		MyLinkedList<Integer> linkedList = new MyLinkedList<>();
-		for (int i = 0; i < 6; i++) {
-			linkedList.add(i);
+		int[] org = { 1, 3, 4, 5, 0, 0, 6, 6, 0, 5, 4, 7, 6, 7, 0, 5 };
+		int[] arr1 = { 3, 5, 7, 8, 9, 10, 123 };
+		int[] arr2 = { 4, 5, 6, 7 };
+		ArrayUtil arrayUtil = new ArrayUtil();
+		int[] res = arrayUtil.getPerfectNumbers(10000);
+//		String string = arrayUtil.join(org, "nishi");
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i]);
 		}
-		linkedList.add(3, 666);
-		linkedList.remove(linkedList.size() - 1);
-		Iterator<Integer> it = linkedList.iterator();
-		while (it.hasNext()) {
-			System.out.println(it.next());
-		}
-		System.out.println("first = " + linkedList.get(0));
+//		System.out.println(string);
 	}
 }
